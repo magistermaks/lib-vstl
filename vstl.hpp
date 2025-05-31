31eaa87037f046589b5a3ab10360ba29ce572b02
magistermaks <magistermaks@darktree.net>
2025-05-31 21:35:07 +0200

    Robust signals

--- vstl.hpp
+++ vstl.hpp
@@ -65,51 +65,68 @@
 #define VSTL_TO_STR(macro) VSTL_STR_VALUE(macro)
 #define VSTL_LINE "on line " VSTL_TO_STR(__LINE__)
 #define VSTL_EXCEPT "Expected exception"
-#define VSTL_RETHROW catch (vstl::TestFail& fail) { throw fail; }
+#define VSTL_RETHROW catch (vstl::test_error& fail) { throw fail; }
 #define VSTL_VTOS(value) + vstl::to_printable(value) +
 
 /// used to define a test of the given [name]: TEST(example_test) { /* the test */ }
-#define TEST(name)          VSTL_BLC  vstl::Test VSTL_UNIQUE(__vstl_test__) = #name+[] ()
+#define TEST(name) \
+VSTL_BLC  vstl::Test VSTL_UNIQUE(__vstl_test__) = #name+[] ()
 
-/// used as a starting point for the VSTL, place anywhere in the test file, preferebly at the end: BEGIN(VSTL_MODE_LENIENT)
-#define BEGIN(mode)         VSTL_BLC  int main() { return vstl::run(std::cout, mode); }
+/// used as a starting point for the VSTL, place anywhere in the test file, preferably at the end: BEGIN(VSTL_MODE_LENIENT)
+#define BEGIN(mode) \
+VSTL_BLC  int main() { return vstl::run(std::cout, mode); }
 
 /// used to defined error handlers (converters), place anywhere in the test file. use like this: HANDLER { CATCH_PTR (my_error_class& err) { FAIL(err.str())  } }
-#define HANDLER             VSTL_BLC  vstl::Handler VSTL_UNIQUE(__vstl_handler__) = "handler"+[] (std::exception_ptr ptr)
+#define HANDLER \
+VSTL_BLC  vstl::Handler VSTL_UNIQUE(__vstl_handler__) = "handler"+[] (std::exception_ptr ptr)
 
 /// helper used in defining error handlers
-#define CATCH_PTR                     try { if(ptr) std::rethrow_exception(ptr); } catch
+#define CATCH_PTR \
+try { if(ptr) std::rethrow_exception(ptr); } catch
 
-/// failes the test with the given [reason] when called
-#define FAIL(reason)                  vstl::fail("" VSTL_VTOS(reason) ", " VSTL_LINE "!");
+/// fails the test with the given [reason] when called
+#define FAIL(reason) \
+vstl::fail("" VSTL_VTOS(reason) ", " VSTL_LINE "!");
 
-/// asserts the [condition] is true, otherwise failes the test with the custom [reason]
-#define ASSERT_MSG(condition, reason) if(!(condition)) FAIL(reason)
+/// asserts the [condition] is true, otherwise fails the test with the custom [reason]
+#define ASSERT_MSG(condition, reason) \
+if(!(condition)) FAIL(reason)
 
-/// asserts the [condition] is true, otherwise failes the test
-#define ASSERT(condition)             ASSERT_MSG(condition, "Expected " #condition " to be true, but it was not")
+/// asserts the [condition] is true, otherwise fails the test
+#define ASSERT(condition) \
+ASSERT_MSG(condition, "Expected " #condition " to be true, but it was not")
 
-/// checks if the [va] equals [vb], otherwise failes the test
-#define CHECK(va, vb)                 VSTL_UNEQUAL(va, vb) FAIL("Expected " VSTL_VTOS(__vstl_a__) " to be equal " VSTL_VTOS(__vstl_b__) ", " #va " != " #vb)
+/// checks if the [va] equals [vb], otherwise fails the test
+#define CHECK(va, vb) \
+VSTL_UNEQUAL(va, vb) FAIL("Expected " VSTL_VTOS(__vstl_a__) " to be equal " VSTL_VTOS(__vstl_b__) ", " #va " != " #vb)
 
-/// checks if the given block [...] throws an exception, otherwise failes the test
-#define EXPECT_ANY(...)               try { __VA_ARGS__; FAIL(VSTL_EXCEPT); } VSTL_RETHROW catch (...) {}
+/// checks if the given block [...] throws an exception, otherwise fails the test
+#define EXPECT_ANY(...) \
+try { __VA_ARGS__; FAIL(VSTL_EXCEPT); } VSTL_RETHROW catch (...) {}
 
-/// checks if the given block [...] throws an exception of the given [type], otherwise failes the test
-#define EXPECT(type, ...)             try{ __VA_ARGS__; FAIL(VSTL_EXCEPT); } VSTL_RETHROW catch (type& t) {} catch (...) { FAIL("Expected exception of type " #type); }
+/// checks if the given block [...] throws an exception of the given [type], otherwise fails the test
+#define EXPECT(type, ...) \
+try{ __VA_ARGS__; FAIL(VSTL_EXCEPT); } VSTL_RETHROW catch (type& t) {} catch (...) { FAIL("Expected exception of type " #type); }
 
 enum TestMode : short {
 
 	/// will skip failed tests
 	VSTL_MODE_LENIENT,
 
-	/// will stop as soon any any test failes
+	/// will stop as soon any any test fails
 	VSTL_MODE_STRICT
 
 };
 
 // internal namespace, don't use :gun:
 namespace vstl {
+
+
+
+	/*
+	 * Those concepts are used to determine how a value can be converted to std::string,
+	 * given a value the vstl::to_printable() WILL return some value based on those concepts
+	 */
 
 	template <typename T>
 	concept ConvertibleToStdString = requires(T value){ std::to_string(value); };
@@ -118,25 +135,32 @@ namespace vstl {
 	concept CastableToStdString = std::convertible_to<T, std::string>;
 
 	template <typename T>
-	concept AppendableToStdString = requires(T value, std::stringstream ss){ ss << value; } && !ConvertibleToStdString<T> && !CastableToStdString<T>;
+	concept AppendableToStdStringStream = requires(T value, std::stringstream ss){ ss << value; } && !ConvertibleToStdString<T> && !CastableToStdString<T>;
+
+
+
+	/*
+	 * The vstl::to_printable() function is used by VSTL to print useful representations of
+	 * tested values, if all fails a generic fallback string will be used
+	 */
 
 	template <typename T>
-	inline std::string to_printable(const T& value) {
+	std::string to_printable(const T& value) {
 		return "<non-printable value>";
 	}
 
 	template <ConvertibleToStdString T>
-	inline std::string to_printable(const T& value) {
+	std::string to_printable(const T& value) {
 		return std::to_string(value);
 	}
 
 	template <CastableToStdString T>
-	inline std::string to_printable(const T& value) {
+	std::string to_printable(const T& value) {
 		return value;
 	}
 
-	template <AppendableToStdString T>
-	inline std::string to_printable(const T& value) {
+	template <AppendableToStdStringStream T>
+	std::string to_printable(const T& value) {
 		std::stringstream ss;
 		ss << value;
 		return ss.str();
@@ -160,9 +184,9 @@ namespace vstl {
 		handlers.push_back(handler);
 	}
 
-	struct TestFail final : public std::runtime_error {
+	struct test_error final : std::runtime_error {
 
-		explicit TestFail(const std::string& error)
+		explicit test_error(const std::string& error)
 		: runtime_error(error) {}
 
 	};
@@ -206,7 +230,7 @@ namespace vstl {
 			try {
 				call(VSTL_TEST_COUNT);
 
-			} catch (vstl::TestFail& fail) {
+			} catch (test_error& fail) {
 				out << "Test '" << this->name << "' " VSTL_FAILED "! Error: " << fail.what() << std::endl;
 				vstl::failed ++;
 				return false;
@@ -218,7 +242,7 @@ namespace vstl {
 				for (const Handler& handler : vstl::handlers) {
 					try {
 						handler.call(ptr);
-					} catch(vstl::TestFail& fail) {
+					} catch(test_error& fail) {
 						out << "Test '" << this->name << "' " VSTL_FAILED "! Error: " << fail.what() << std::endl;
 						vstl::failed ++;
 						return false;
@@ -227,7 +251,7 @@ namespace vstl {
 					}
 				}
 
-				// everything has failed us, just try to print some reason
+				// everything has failed us, just try to print *some* reason
 				try {
 					out << "Test '" << this->name << "' " VSTL_FAILED "! Unregistered exception thrown! ";
 					std::rethrow_exception(ptr);
@@ -256,7 +280,7 @@ namespace vstl {
 		size_t executed = vstl::failed + vstl::successful;
 		double millis = std::chrono::duration<double, std::milli>(time).count();
 
-		out << std::endl << "Executed " << executed << " ";
+		out << std::endl << std::dec << "Executed " << executed << " ";
 		out << (executed == 1 ? "test" : "tests") << ", ";
 		out << vstl::failed << " failed, ";
 		out << vstl::successful << " succeeded.";
@@ -264,14 +288,31 @@ namespace vstl {
 		out << std::endl;
 	}
 
+	const char* signal_name(int sig) {
+		if (sig == SIGSEGV) return "SIGSEGV";
+		if (sig == SIGILL) return "SIGILL";
+		if (sig == SIGFPE) return "SIGFPE";
+		if (sig == SIGTRAP) return "SIGTRAP";
+
+		return "unknown signal";
+	}
+
 	#ifdef _WIN32
+	void print_signal(const char* test, int sig) {
+		printf("Test '%s' " VSTL_FAILED "! Error: Received %s (#%d)!\n", test, signal_name(sig), sig);
+	}
+
 	void signal_handler(int sig) {
-		printf("Test '%s' " VSTL_FAILED "! Error: Received SIGSEGV!\n", tests[test_id].name);
+		print_signal(tests[test_id].name, sig);
 		longjmp(vstl::jmp, 1);
 	}
 	#else
+	void print_signal(const char* test, int sig, int64_t address) {
+		printf("Test '%s' " VSTL_FAILED "! Error: Received %s (#%d) while trying to access: 0x%lx!\n", test, signal_name(sig), sig, address);
+	}
+
 	void signal_handler(int sig, siginfo_t* si, void* unused) {
-		printf("Test '%s' " VSTL_FAILED "! Error: Received SIGSEGV while trying to access: 0x%lx!\n", tests[test_id].name, (long) si->si_addr);
+		print_signal(tests[test_id].name, sig, reinterpret_cast<int64_t>(si->si_addr));
 		siglongjmp(vstl::jmp, 1);
 	}
 	#endif
@@ -280,16 +321,31 @@ namespace vstl {
 
 		#ifdef _WIN32
 			signal(SIGSEGV, signal_handler);
+			signal(SIGILL, signal_handler);
+			signal(SIGFPE, signal_handler);
+			signal(SIGTRAP, signal_handler);
 		#else
+
+			// custom stack to make VSTL more resilient if the stack
+			// pointer gets corrupted
+			stack_t stack;
+			stack.ss_sp = malloc(SIGSTKSZ);
+			stack.ss_size = SIGSTKSZ;
+			stack.ss_flags = 0;
+			sigaltstack(&stack, nullptr);
+
+			// our signal action description
 			struct sigaction action;
 			action.sa_flags = SA_SIGINFO;
+			action.sa_flags = SA_SIGINFO | SA_NODEFER | SA_ONSTACK;
 			sigemptyset(&action.sa_mask);
 			action.sa_sigaction = signal_handler;
 
-			if (sigaction(SIGSEGV, &action, NULL) == -1) {
-				out << "WARN: Failed to setup SIGSEGV handler!";
-				out << std::endl;
-			}
+			// registers for all applicable signals
+			sigaction(SIGSEGV, &action, nullptr);
+			sigaction(SIGILL, &action, nullptr);
+			sigaction(SIGFPE, &action, nullptr);
+			sigaction(SIGTRAP, &action, nullptr);
 		#endif
 
 		const auto start = std::chrono::steady_clock::now();
@@ -298,7 +354,7 @@ namespace vstl {
 			#ifdef _WIN32
 			if (setjmp(jmp)) {
 			#else
-			if (sigsetjmp(jmp, 1)) {
+			if (sigsetjmp(jmp, 0xffffffff)) {
 			#endif
 				failed ++;
 				goto skip;
@@ -327,15 +383,15 @@ namespace vstl {
 
 	template<typename S>
 	void fail(const S& message) {
-		throw TestFail {message};
+		throw test_error {message};
 	}
 
 }
 
-vstl::Test operator +(const char* name, const vstl::Test::Func& tester) {
+inline vstl::Test operator +(const char* name, const vstl::Test::Func& tester) {
     return vstl::Test {name, tester};
 }
 
-vstl::Handler operator +(const char* name, const vstl::Handler::Func& handler) {
+inline vstl::Handler operator +(const char* name, const vstl::Handler::Func& handler) {
     return vstl::Handler {handler};
 }
